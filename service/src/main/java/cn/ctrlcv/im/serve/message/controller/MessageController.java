6fad6c02f31c6c1c2d6bbdae8fe793a25f95d590
liujm <15968396335@163.com>
2023-05-26 10:29:53 +0800

    数据同步-拉取离线消息

--- service/src/main/java/cn/ctrlcv/im/serve/message/controller/MessageController.java
+++ service/src/main/java/cn/ctrlcv/im/serve/message/controller/MessageController.java
@@ -1,13 +1,19 @@
 package cn.ctrlcv.im.serve.message.controller;
 
 import cn.ctrlcv.im.common.ResponseVO;
+import cn.ctrlcv.im.common.model.SyncReq;
+import cn.ctrlcv.im.common.model.SyncResp;
 import cn.ctrlcv.im.common.model.message.CheckSendMessageReq;
+import cn.ctrlcv.im.common.model.message.OfflineMessageContent;
 import cn.ctrlcv.im.serve.message.model.request.GroupSendMessageReq;
 import cn.ctrlcv.im.serve.message.model.request.P2pSendMessageReq;
 import cn.ctrlcv.im.serve.message.service.GroupMessageService;
+import cn.ctrlcv.im.serve.message.service.MessageSyncService;
 import cn.ctrlcv.im.serve.message.service.P2pMessageService;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 /**
  * Class Name: MessageController
@@ -23,6 +29,9 @@ public class MessageController {
     private final P2pMessageService p2pMessageService;
     private final GroupMessageService groupMessageService;
 
+    @Resource
+    private MessageSyncService messageSyncService;
+
     public MessageController(P2pMessageService p2pMessageService, GroupMessageService groupMessageService) {
         this.p2pMessageService = p2pMessageService;
         this.groupMessageService = groupMessageService;
@@ -30,7 +39,8 @@ public class MessageController {
 
     /**
      * 发送单聊消息
-     * @param req 请求参数
+     *
+     * @param req   请求参数
      * @param appId 应用id
      * @return 响应参数
      */
@@ -42,22 +52,23 @@ public class MessageController {
 
     /**
      * 发送群聊消息
-     * @param req 请求参数
-     * @param appId 应用id
+     *
+     * @param req        请求参数
+     * @param appId      应用id
      * @param identifier 操作者
      * @return 响应参数
      */
     @PostMapping("/group/message")
-    public ResponseVO sendMessage(@RequestBody @Validated GroupSendMessageReq req, Integer appId, String identifier)  {
+    public ResponseVO sendMessage(@RequestBody @Validated GroupSendMessageReq req, Integer appId, String identifier) {
         req.setAppId(appId);
         req.setOperator(identifier);
         return ResponseVO.successResponse(groupMessageService.send(req));
     }
 
 
-
     /**
      * 检查消息发送权限
+     *
      * @param req 请求参数
      * @return 响应参数
      */
@@ -66,6 +77,20 @@ public class MessageController {
         return ResponseVO.successResponse(p2pMessageService.checkImServicePermission(req.getFromId(), req.getToId(), req.getAppId()));
     }
 
+    /**
+     * 同步用户的离线消息
+     *
+     * @param req        {@link SyncReq}
+     * @param appId      应用id
+     * @param identifier 用户ID
+     * @return resp {@link SyncResp}
+     */
+    @PostMapping("/syncOfflineMessage")
+    public ResponseVO<SyncResp<OfflineMessageContent>> syncOfflineMessage(@RequestBody @Validated SyncReq req, Integer appId, String identifier) {
+        req.setAppId(appId);
+        req.setOperator(identifier);
+        return ResponseVO.successResponse(messageSyncService.syncOfflineMessage(req));
+    }
 
 
 }
--- service/src/main/java/cn/ctrlcv/im/serve/message/service/MessageSyncService.java
+++ service/src/main/java/cn/ctrlcv/im/serve/message/service/MessageSyncService.java
@@ -1,17 +1,30 @@
 package cn.ctrlcv.im.serve.message.service;
 
 import cn.ctrlcv.im.codec.pack.message.MessageReadedPack;
+import cn.ctrlcv.im.common.constant.Constants;
 import cn.ctrlcv.im.common.enums.command.Command;
 import cn.ctrlcv.im.common.enums.command.GroupEventCommand;
 import cn.ctrlcv.im.common.enums.command.MessageCommand;
+import cn.ctrlcv.im.common.model.SyncReq;
+import cn.ctrlcv.im.common.model.SyncResp;
 import cn.ctrlcv.im.common.model.message.MessageReadedContent;
 import cn.ctrlcv.im.common.model.message.MessageReceiverAckContent;
+import cn.ctrlcv.im.common.model.message.OfflineMessageContent;
 import cn.ctrlcv.im.serve.conversation.service.ConversationService;
 import cn.ctrlcv.im.serve.utils.MessageProducer;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.redis.core.DefaultTypedTuple;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 /**
  * Class Name: MessageSyncService
@@ -28,6 +41,9 @@ public class MessageSyncService {
 
     @Resource
     private ConversationService conversationService;
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
 
     /**
      * 消息接收确认
@@ -81,4 +97,41 @@ public class MessageSyncService {
         messageProducer.sendToUser(readedContent.getFromId(), command, readedPack, readedContent);
     }
 
+    /**
+     * 离线消息同步
+     *
+     * @param req {@link SyncReq}
+     * @return {@link SyncResp}
+     */
+    public SyncResp<OfflineMessageContent> syncOfflineMessage(SyncReq req) {
+        SyncResp<OfflineMessageContent> resp = new SyncResp<>();
+
+        String key = req.getAppId() + ":" + Constants.RedisKey.OFFLINE_MESSAGE + ":" + req.getOperator();
+        // 获取离线消息最大的sequence
+        long maxSeq = 0L;
+        ZSetOperations<String, Object> opsForZSet = redisTemplate.opsForZSet();
+        Set<ZSetOperations.TypedTuple<Object>> set = opsForZSet.reverseRangeWithScores(key, 0, 0);
+        if (!CollectionUtils.isEmpty(set)) {
+            List<ZSetOperations.TypedTuple<Object>> list = new ArrayList<>(set);
+            DefaultTypedTuple<Object> typedTuple = (DefaultTypedTuple<Object>) list.get(0);
+            maxSeq = Objects.requireNonNull(typedTuple.getScore()).longValue();
+        }
+
+        List<OfflineMessageContent> list = new ArrayList<>();
+        resp.setMaxSequence(maxSeq);
+        Set<ZSetOperations.TypedTuple<Object>> querySet = opsForZSet
+                .rangeByScoreWithScores(key, req.getLastSequence(), maxSeq, 0, req.getMaxLimit());
+        for (ZSetOperations.TypedTuple<Object> typedTuple : querySet) {
+            String value = String.valueOf(typedTuple.getValue());
+            OfflineMessageContent messageContent = JSONObject.parseObject(value, OfflineMessageContent.class);
+            list.add(messageContent);
+        }
+        resp.setDataList(list);
+        if (!CollectionUtils.isEmpty(list)) {
+            OfflineMessageContent content = list.get(list.size() - 1);
+            resp.setCompleted(maxSeq <= content.getMessageKey());
+        }
+
+        return resp;
+    }
 }
